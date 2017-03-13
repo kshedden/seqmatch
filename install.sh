d15929017de2bb61c3d52aa27d5a207ef654e9db
Kerby A Shedden <kshedden@umich.edu>
2017-03-12 22:32:44 -0400

    update

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ go get -u github.com/kshedden/seqmatch/window_reads
 go get -u github.com/kshedden/seqmatch/bloom
 go get -u github.com/kshedden/seqmatch/merge_bloom
 go get -u github.com/kshedden/seqmatch/runmatch
+go get -u github.com/kshedden/sztool
+
 
